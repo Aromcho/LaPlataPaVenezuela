fdc96fbac2a5d55e172b3ce39bb1c4777aebb931
root <root@srv570096.hstgr.cloud>
2024-10-14 23:40:07 +0000

    rama de desarrollo

--- client/src/pages/Admin/ProductManagement/ProductManagement.jsx
+++ client/src/pages/Admin/ProductManagement/ProductManagement.jsx
@@ -25,6 +25,7 @@ const ProductManagement = () => {
       try {
         let url = `/api/product/paginate?limit=15&page=1`;
         const response = await fetch(url);
+        console.log('products:', response);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
