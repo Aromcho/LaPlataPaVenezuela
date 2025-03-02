755e075fa1f9fa235ae5db9fafbee013fba84597
Aromcho <barriosarom@gmail.com>
2025-03-02 10:41:05 -0300

    pasando a produccion

--- client/src/components/Recarga/Recarga.jsx
+++ client/src/components/Recarga/Recarga.jsx
@@ -35,13 +35,9 @@ const Recarga = () => {
                     <div className='metodos red'>
                         <h4>Cambia</h4>
                         <p>Entregamos efectivo</p>
-                        <div className='metodo-logo-cont'>
-                            <img className='metodo-logo' src="./img/Wally.png" alt="" />
-                            <h4>Wally</h4>
-                        </div>
-                        <div className='metodo-logo-cont'>
-                            <img className='metodo-logo' src="./img/Zinli.png" alt="" />
-                            <h4>Zinli</h4>
+                        <div className='metodo-logo-cont-b'>
+                            <img className='metodo-logo' src="./img/binance.png" alt="" />
+                            <h4>USDT</h4>
                         </div>
                     </div>
                 </div>
--- client/src/components/Testimonials/Testimonials.jsx
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
       name: 'Juan Pérez',
       city: 'Ciudad de México',
       comment:
-        '¡El servicio de envío de dinero más rápido y seguro que he usado! Recomiendo totalmente Legz.',
+        'Me encanta que sé qie mis padres reciben el dinero de forma rápida y segura.',
     },
     {
       id: 2,
@@ -18,12 +18,85 @@ const Testimonials = () => {
       city: 'Madrid, España',
       comment: 'plata pa venezuela hizo que enviar dinero a mi familia en América Latina fuera fácil y sin complicaciones.',
     },
+    {
+      id: 3,
+      name: 'Maria Rodriguez',
+      city: 'Madrid, España',
+      comment: 'La atencion es muy buena, tube dudas he hicimos una videollamada.',
+    },
   ];
 
   return (
     <div className="testimonials-container">
-      <h2 className="text-center mb-4 py-3">LO QUE DICEN QUIENES CONFIAN EN NOSOTROS</h2>
-      
+      <h2 className="testimonials-title">LO QUE DICEN QUIENES CONFIAN EN NOSOTROS</h2>
+      <div className="estimonials-videos">
+        <Container>
+          <Row className="mb-4">
+            <Col md={6}>
+              <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/videoid1"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Col>
+            <Col md={6}>
+              <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/videoid2"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/videoid3"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Col>
+            <Col md={6}>
+              <iframe
+                width="100%"
+                height="315"
+                src="https://www.youtube.com/embed/videoid4"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonios-cards">
+        {testimonials.map((testimonial) => (
+          <div key={testimonial.id} className="testimonio-card">
+              <img className='avatar' src="./img/avatar1.png" alt="" />
+              <Card.Title>{testimonial.name}</Card.Title>
+              <Card.Text>{testimonial.comment}</Card.Text>
+              <img className='estrellas' src="./img/estrellas.png" alt="" />
+          </div>
+        ))}
+        <div className="testimonio-rayas">
+          <img src="./img/raya1.png" alt="" />
+          <img src="./img/raya2.png" alt="" />
+          <img src="./img/raya1.png" alt="" />
+        </div>
+
+      </div>
     </div>
   );
 };
