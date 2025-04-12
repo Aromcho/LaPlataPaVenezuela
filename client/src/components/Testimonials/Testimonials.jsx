75d9a3e73095cc5dd7e5849d08fd362295edbdc6
Aromcho <barriosarom@gmail.com>
2025-04-12 19:32:42 -0300

    testimonials listo

--- client/src/components/Testimonials/Testimonials.jsx
+++ client/src/components/Testimonials/Testimonials.jsx
@@ -7,84 +7,56 @@ const Testimonials = () => {
   const testimonials = [
     {
       id: 1,
-      name: 'Juan Pérez',
-      city: 'Ciudad de México',
+      name: 'Mariangeles Leon',
+      image: './img/MariangelesLeon.jpg',
       comment:
-        'Me encanta que sé qie mis padres reciben el dinero de forma rápida y segura.',
+        'Gracias a “La Plata pa’ Venezuela” por ayudarme a estar más cerca de mi familia; siempre hacen que el envío de remesas sea rápido, seguro y sin complicaciones. ¡1000% recomendados! Gracias por ser ese puente confiable con nuestra gente en Venezuela.🙌🏻',
     },
     {
       id: 2,
-      name: 'Maria Rodriguez',
-      city: 'Madrid, España',
-      comment: 'plata pa venezuela hizo que enviar dinero a mi familia en América Latina fuera fácil y sin complicaciones.',
+      name: 'Yaina Rodríguez',
+      image: './img/YainaRodríguez.jpg',
+      comment: 'En principio te conocí por  allá 2018 o comienzos del 2019, si mal no recuerdo!!! Desde el primer momento has sido profesional y transparente! Te super agradezco tu existencia por que la verdad haces que la vida sea más fácil y práctica! Contigo siempre ha sido un transacción confiable!! Y por supuesto Recomendable! Gracias Gracias Gracias!!!',
     },
     {
       id: 3,
-      name: 'Maria Rodriguez',
-      city: 'Madrid, España',
-      comment: 'La atencion es muy buena, tube dudas he hicimos una videollamada.',
+      name: 'Arom Aguilar',
+      image: './img/AromAguilar.jpg',
+      comment: 'Conocí Plata pa’ Venezuela en 2019 y, desde entonces, no los suelto. Todo va al grano y sin enredos: mando la plata y en cuestión de horas le llega a mi gente. Me ahorro dolores de cabeza y sé que el dinero está seguro. Si alguien me pregunta, los recomiendo con los ojos cerrados.',
     },
+  ];
+
+  // Array de videos locales
+  const videos = [
+    './videos/video1.MOV',
+    './videos/video2.mp4',
+    './videos/video3.mp4',
+    './videos/video4.mp4'
   ];
 
   return (
     <div className="testimonials-container">
       <h2 className="testimonials-title">LO QUE DICEN QUIENES CONFIAN EN NOSOTROS</h2>
-      <div className="estimonials-videos">
+      <div className="testimonials-videos">
         <Container>
-          <Row className="mb-4">
-            <Col md={6}>
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/videoid1"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </Col>
-            <Col md={6}>
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/videoid2"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={6}>
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/videoid3"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </Col>
-            <Col md={6}>
-              <iframe
-                width="100%"
-                height="315"
-                src="https://www.youtube.com/embed/videoid4"
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </Col>
+          <Row className="gy-4">
+            {videos.map((src, i) => (
+              <Col key={i} md={6}>
+                <div className="video-wrapper ratio ratio-16x9">
+                  <video className="testimonial-video" controls>
+                    <source src={src} type="video/mp4" />
+                    Tu navegador no soporta videos HTML5.
+                  </video>
+                </div>
+              </Col>
+            ))}
           </Row>
         </Container>
       </div>
       <div className="testimonios-cards">
         {testimonials.map((testimonial) => (
           <div key={testimonial.id} className="testimonio-card">
-              <img className='avatar' src="./img/avatar1.png" alt="" />
+              <img className='avatar' src={testimonial.image} alt="" />
               <Card.Title>{testimonial.name}</Card.Title>
               <Card.Text>{testimonial.comment}</Card.Text>
               <img className='estrellas' src="./img/estrellas.png" alt="" />
@@ -95,7 +67,6 @@ const Testimonials = () => {
           <img src="./img/raya2.png" alt="" />
           <img src="./img/raya1.png" alt="" />
         </div>
-
       </div>
     </div>
   );
